537c81cbb7794413cb4ee8200540a1948f06a793
hperez30 <hperez@issi.uned.es>
2023-12-17 12:07:27 +0100

    refactoring

--- coreanddead/core_test/test.c
+++ coreanddead/core_test/test.c
@@ -1,18 +1,8 @@
 #include "test.h"
 
 int main(int argc, char **argv)
-{
-	/* Initilize buddy */
-	bdd_init(INITBDDNODES, INITBDDCACHE);
-	bdd_setmaxincrease(INITBDDMAXINCREASE);
-
-	struct timeval t_ini, t_fin;
-	bdd solutionSpace;
-	
-	int *num_dead_core;
-
-	if (argc != 3)
-	{
+{	
+	if (argc != 3) {
 		std::cerr << "Usage: " << argv[0] << " <bdd file> <option method>" << std::endl;
 		exit(-1);
 	}
@@ -20,16 +10,20 @@ int main(int argc, char **argv)
 	std::ifstream mybdd(argv[1]);
 	int method = atoi(argv[2]);
 
-	if (!mybdd)
-	{
+	if (!mybdd) {
 		std::cerr << "File " << argv[1] << " not found" << std::endl;
 		exit(-1);
 	}
 
+	/* Initilize buddy */
+	bdd_init(INITBDDNODES, INITBDDCACHE);
+	bdd_setmaxincrease(INITBDDMAXINCREASE);
 #ifdef REORDER
 	/* compile with debug flag if you want to use automatic reordering techniques */
 	bdd_autoreorder(BDD_REORDER_SIFT);
 #endif
+
+	bdd solutionSpace;
 
 	bdd_fnload((char *)argv[1], solutionSpace);
 
@@ -37,56 +31,56 @@ int main(int argc, char **argv)
 
 	bdd_varblockall();
 	bdd_reorder(BDD_REORDER_SIFT);
-
+	
+	struct timeval t_ini, t_fin;
 	gettimeofday(&t_ini, NULL);
 
-	switch (method)
-	{
-	case 1: // brute force approach
-		num_dead_core = (int *)malloc(2*sizeof(int));
-		for (int i=0; i < 2; ++i)
-			num_dead_core[i] = 0;
+	switch (method) {
+		case 1: // brute force approach
+			int *num_dead_core = (int *)malloc(2*sizeof(int));
+			for (int i=0; i < 2; ++i)
+				num_dead_core[i] = 0;
 
-		for (int j = 0; j < bdd_varnum(); ++j)
-		{
-			if ((solutionSpace & bdd_nithvar(j)) == bdd_false())
-				num_dead_core[1]++;
-			else if ((solutionSpace & bdd_ithvar(j)) == bdd_false())
-				num_dead_core[0]++;
-		}
+			for (int j = 0; j < bdd_varnum(); ++j)
+			{
+				if ((solutionSpace & bdd_nithvar(j)) == bdd_false())
+					num_dead_core[1]++;
+				else if ((solutionSpace & bdd_ithvar(j)) == bdd_false())
+					num_dead_core[0]++;
+			}
 
-		show_results(num_dead_core[0], num_dead_core[1]);
-		gettimeofday(&t_fin, NULL);
-		show_elapsed_time(&t_fin, &t_ini);
+			show_results(num_dead_core[0], num_dead_core[1]);
+			gettimeofday(&t_fin, NULL);
+			show_elapsed_time(&t_fin, &t_ini);
+			
+			free(num_dead_core);
+			break;
 
-		break;
+		case 2: // our approach			
+			int *var_low = (int *)calloc(bdd_varnum(), sizeof(int));
+			int *var_high = (int *)calloc(bdd_varnum(), sizeof(int));
 
-	case 2: // our approach
-		int *var_low, *var_high, *marks, *res_node; 
-		var_low = (int *)calloc(bdd_varnum(), sizeof(int));
-		var_high = (int *)calloc(bdd_varnum(), sizeof(int));
+			int *marks = (int *)calloc(bddnodesize, sizeof(int));
+			int *res_node = (int *)calloc(bddnodesize, sizeof(int));
 
-		marks = (int *)calloc(bddnodesize, sizeof(int));
-		res_node = (int *)calloc(bddnodesize, sizeof(int));
+			get_dependencies_conflicts(solutionSpace, var_low, var_high, marks, res_node);
 
-		get_dependencies_conflicts(solutionSpace, var_low, var_high, marks, res_node);
+			int *num_dead_core = (int *)malloc(2*sizeof(int));
+			num_dead_core = dead_features(var_high, var_low, bdd_varnum());
 
-		num_dead_core = (int *)malloc(2*sizeof(int));
-		num_dead_core = dead_features(var_high, var_low, bdd_varnum());
+			show_results(num_dead_core[0], num_dead_core[1]);
+			gettimeofday(&t_fin, NULL);
+			show_elapsed_time(&t_fin, &t_ini);
 
-		show_results(num_dead_core[0], num_dead_core[1]);
-		gettimeofday(&t_fin, NULL);
-		show_elapsed_time(&t_fin, &t_ini);
+			free(var_low);
+			free(var_high);
+			free(marks);
+			free(res_node);
+			free(num_dead_core);
+			break;
 
-		free(var_low);
-		free(var_high);
-		free(marks);
-		free(res_node);
-		free(num_dead_core);
-		break;
-
-	default:
-		std::cout << "invalid option " << method << std::endl;
+		default:
+			std::cout << "invalid option " << method << std::endl;
 
 	} // end switch
 
@@ -95,20 +89,17 @@ int main(int argc, char **argv)
 } // end main
 
 /* return "a - b" in seconds */
-double timeval_diff(struct timeval *a, struct timeval *b)
-{
+double timeval_diff(struct timeval *a, struct timeval *b) {
 	return (double)(a->tv_sec + (double)a->tv_usec / 1000000) -
 		   (double)(b->tv_sec + (double)b->tv_usec / 1000000);
 } // time_diff
 
-void show_results(int num_dead, int num_core)
-{
+void show_results(int num_dead, int num_core) {
 	std::cout << "core: " << num_core << std::endl;
 	std::cout << "dead: " << num_dead << std::endl;
 }
 
-void show_elapsed_time(struct timeval *t_fin, struct timeval *t_ini)
-{
+void show_elapsed_time(struct timeval *t_fin, struct timeval *t_ini){
 	double secs;
 
 	secs = timeval_diff(t_fin, t_ini);
