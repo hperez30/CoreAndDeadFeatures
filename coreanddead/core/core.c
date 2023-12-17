8cb48e7cc94ef64933b4de226b27bfd44f290c81
hperez30 <hperez@issi.uned.es>
2023-12-17 11:36:03 +0100

    version 1.1.0

--- coreanddead/core/core.c
+++ coreanddead/core/core.c
@@ -1,99 +1,106 @@
-using namespace std;
-#include "core.h"
+#include <core.h>
 
-void dead_features(int *var_high, int *var_low, int num_var)
+int* dead_features(int *var_high, int *var_low, int num_var)
 {
-	int num_dead, num_core;
-	
-	num_dead = 0;num_core = 0;
-	
-	for (int i=0; i<num_var; ++i)
+	int *num_dead_core = (int *)malloc(2*sizeof(int));
+	for (int i = 0; i < 2; ++i)
+		num_dead_core[i] = 0;
+
+	for (int i = 0; i < num_var; ++i)
 	{
 		if (var_high[i] != var_low[i])
+		{
 			if (var_high[i] && !var_low[i])
 			{
-				fprintf(stdout, "core %d \n", i);
-				num_core++;
-			} else {
+#ifdef DEBUG
+				/* compile with debug flag */				
+				std::cout << "var " << i "is core" << endl;
+#endif
+				num_dead_core[1]++;
+			}
+			else
+			{
 				if (!var_high[i] && var_low[i])
 				{
-					fprintf(stdout, "dead %d \n", i);
-					num_dead++;
+#ifdef DEBUG
+					/* compile with debug flag */
+					std::cout << "var " << i "is dead" << endl;
+#endif
+					num_dead_core[0]++;
 				}
 			}
+		}			
 	}
-	
-	fprintf(stdout, "Number of core feaures: %d \nNumber of dead features: %d \n", num_core, num_dead);
-}//end dead_features
 
-int get_dependencies_conflicts(const bdd r, int *vars_low, int *vars_high, int *marks,  int *res_node)
-{
-    	int level, level_high, level_low;
-    	int root;
-    
-     	root = r.id();
+	return num_dead_core;
+} // end dead_features
+
+int get_dependencies_conflicts(const bdd r, int *vars_low, int *vars_high, int *marks, int *res_node)
+{	
+	int root = r.id();
 	if (!marks[root])
 	{
 		marks[root] = !marks[root];
-		level = bdd_var2level(bdd_var(r));
-		if (bdd_high(r).id() == 1) 
+		int level = bdd_var2level(bdd_var(r));
+		if (bdd_high(r).id() == 1)
 		{
-			vars_high[level] = 1;		
-			for (int i=level+1; i < bdd_varnum(); ++i) 
-			{							
+			vars_high[level] = 1;
+			for (int i = level + 1; i < bdd_varnum(); ++i)
+			{
 				vars_high[i] = 1;
 				vars_low[i] = 1;
 			}
 			res_node[root] = 1;
-		} else 
+		}
+		else
 		{
 			if (bdd_high(r).id() != 0)
 			{
 				res_node[root] = get_dependencies_conflicts(bdd_high(r), vars_low, vars_high, marks, res_node);
-							
+
 				if (res_node[root])
-				{								
-					vars_high[level] =  1;
-					level_high = bdd_var2level(bdd_var(bdd_high(r)));
-					for (int i=level+1; i < level_high; ++i) 
-					{			
+				{
+					vars_high[level] = 1;
+					int level_high = bdd_var2level(bdd_var(bdd_high(r)));
+					for (int i = level + 1; i < level_high; ++i)
+					{
 						vars_high[i] = 1;
 						vars_low[i] = 1;
 					}
 				}
-			} 
+			}
 		}
-		
-		if (bdd_low(r).id() == 1) 
+
+		if (bdd_low(r).id() == 1)
 		{
-			vars_low[level] = 1;			
-			for (int i=level+1; i < bdd_varnum(); ++i)
-			{  
+			vars_low[level] = 1;
+			for (int i = level + 1; i < bdd_varnum(); ++i)
+			{
 				vars_high[i] = 1;
 				vars_low[i] = 1;
 			}
 			res_node[root] = 1;
-		} else 
+		}
+		else
 		{
 			if (bdd_low(r).id() != 0)
-			{		
+			{
 				res_node[root] = get_dependencies_conflicts(bdd_low(r), vars_low, vars_high, marks, res_node);
-				
-				if (res_node[root]) 
+
+				if (res_node[root])
 				{
-					vars_low[level] =  1;			
-					level_low = bdd_var2level(bdd_var(bdd_low(r)));
-					
-					for (int i=level+1; i < level_low; ++i) 
-					{			
+					vars_low[level] = 1;
+					int level_low = bdd_var2level(bdd_var(bdd_low(r)));
+
+					for (int i = level + 1; i < level_low; ++i)
+					{
 						vars_high[i] = 1;
 						vars_low[i] = 1;
 					}
 				}
 			}
-		}		
-	} 
+		}
+	}
 
-	return res_node[root];	
-}//end get_dependencies_conflicts
-
+	return res_node[root];
+} // end get_dependencies_conflicts
--- coreanddead/core_test/test.c
+++ coreanddead/core_test/test.c
@@ -1,111 +1,116 @@
-using namespace std;
-#include <iostream>
-#include <fstream>
-#include <sys/time.h>
+#include "test.h"
 
-#include "bdd.h"
-#include "kernel.h"
-#include "core.h"
+int main(int argc, char **argv)
+{
+	/* Initilize buddy */
+	bdd_init(INITBDDNODES, INITBDDCACHE);
+	bdd_setmaxincrease(INITBDDMAXINCREASE);
 
-const int INITBDDNODES       = 50000;
-const int INITBDDCACHE       = 10000000;
-const int INITBDDMAXINCREASE = 30000;
+	struct timeval t_ini, t_fin;
+	bdd solutionSpace;
+	
+	int *num_dead_core;
+
+	if (argc != 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " <bdd file> <option method>" << std::endl;
+		exit(-1);
+	}
+
+	std::ifstream mybdd(argv[1]);
+	int method = atoi(argv[2]);
+
+	if (!mybdd)
+	{
+		std::cerr << "File " << argv[1] << " not found" << std::endl;
+		exit(-1);
+	}
+
+#ifdef REORDER
+	/* compile with debug flag if you want to use automatic reordering techniques */
+	bdd_autoreorder(BDD_REORDER_SIFT);
+#endif
+
+	bdd_fnload((char *)argv[1], solutionSpace);
+
+	std::cerr << "File " << argv[1] << " loaded" << std::endl;
+
+	bdd_varblockall();
+	bdd_reorder(BDD_REORDER_SIFT);
+
+	gettimeofday(&t_ini, NULL);
+
+	switch (method)
+	{
+	case 1: // brute force approach
+		num_dead_core = (int *)malloc(2*sizeof(int));
+		for (int i=0; i < 2; ++i)
+			num_dead_core[i] = 0;
+
+		for (int j = 0; j < bdd_varnum(); ++j)
+		{
+			if ((solutionSpace & bdd_nithvar(j)) == bdd_false())
+				num_dead_core[1]++;
+			else if ((solutionSpace & bdd_ithvar(j)) == bdd_false())
+				num_dead_core[0]++;
+		}
+
+		show_results(num_dead_core[0], num_dead_core[1]);
+		gettimeofday(&t_fin, NULL);
+		show_elapsed_time(&t_fin, &t_ini);
+
+		break;
+
+	case 2: // our approach
+		int *var_low, *var_high, *marks, *res_node; 
+		var_low = (int *)calloc(bdd_varnum(), sizeof(int));
+		var_high = (int *)calloc(bdd_varnum(), sizeof(int));
+
+		marks = (int *)calloc(bddnodesize, sizeof(int));
+		res_node = (int *)calloc(bddnodesize, sizeof(int));
+
+		get_dependencies_conflicts(solutionSpace, var_low, var_high, marks, res_node);
+
+		num_dead_core = (int *)malloc(2*sizeof(int));
+		num_dead_core = dead_features(var_high, var_low, bdd_varnum());
+
+		show_results(num_dead_core[0], num_dead_core[1]);
+		gettimeofday(&t_fin, NULL);
+		show_elapsed_time(&t_fin, &t_ini);
+
+		free(var_low);
+		free(var_high);
+		free(marks);
+		free(res_node);
+		free(num_dead_core);
+		break;
+
+	default:
+		std::cout << "invalid option " << method << std::endl;
+
+	} // end switch
+
+	bdd_done();
+
+} // end main
 
 /* return "a - b" in seconds */
 double timeval_diff(struct timeval *a, struct timeval *b)
 {
-  return
-    (double)(a->tv_sec + (double)a->tv_usec/1000000) -
-    (double)(b->tv_sec + (double)b->tv_usec/1000000);
-}//time_diff
+	return (double)(a->tv_sec + (double)a->tv_usec / 1000000) -
+		   (double)(b->tv_sec + (double)b->tv_usec / 1000000);
+} // time_diff
 
-int main(int argc, char **argv)  
+void show_results(int num_dead, int num_core)
 {
-	/* Initilize buddy */
-	bdd_init(INITBDDNODES,INITBDDCACHE);
-    bdd_setmaxincrease(INITBDDMAXINCREASE);
-    
-    struct timeval t_ini, t_fin;
-    double secs;
-    
-	bdd solutionSpace;
-    
-    int core, dead, num_test;
-    int *var_low, *var_high;
-    int *marks, *res_node;
-    
-    if (argc != 3) 
-    {
-       cerr << "Usage: " << argv[0]
-            << " <bdd file> <option method>" << endl;
-        exit(-1);
-    }
-   
-    ifstream mybdd(argv[1]);
-    num_test = atoi(argv[2]);
-    
-    if (!mybdd) 
-    {
-        cerr << "File " << argv[1] << " not found" << endl;
-        exit(-1);
-    }
-    
-    /* delete comment if you want to use automatic reordering techniques*/
-    //bdd_autoreorder(BDD_REORDER_SIFT);
-    
-    /*load the bdd*/
-	bdd_fnload((char *)argv[1], solutionSpace);
-	
-	cout << "File " << argv[1] << " loaded" << endl;
-	
-	bdd_varblockall();
-	bdd_reorder(BDD_REORDER_SIFT);
-		
-	//Initialize time	
-    gettimeofday(&t_ini, NULL);		
-    
-    switch(num_test)
-    {
-	  case 1://brute force approach
-	    
-		core=0;dead=0;	
-	    
-		for (int j=0; j<bdd_varnum(); ++j)
-		{
-			if ((solutionSpace&bdd_nithvar(j)) == bdd_false()) core++;
-			else if ((solutionSpace&bdd_ithvar(j)) == bdd_false()) dead++;
-		}
+	std::cout << "core: " << num_core << std::endl;
+	std::cout << "dead: " << num_dead << std::endl;
+}
 
-		printf("core %d \n",core);
-		printf("dead %d\n",dead);
-		
-		gettimeofday(&t_fin, NULL);
-		secs = timeval_diff(&t_fin, &t_ini);
-	
-		fprintf(stdout, "time in %.16g seconds\n", secs);
-		break;
-		
-	  case 2://our approach
-	 
-		var_low = (int*)calloc(bdd_varnum(), sizeof(int));
-		var_high = (int*)calloc(bdd_varnum(), sizeof(int));
-		marks = (int*)calloc(bddnodesize, sizeof(int));
-		res_node = (int*)calloc(bddnodesize, sizeof(int));
-		
-		get_dependencies_conflicts(solutionSpace, var_low, var_high, marks, res_node);
-		
-		dead_features(var_high, var_low, bdd_varnum());
-		gettimeofday(&t_fin, NULL);
-		secs = timeval_diff(&t_fin, &t_ini);
-	
-		fprintf(stdout, "time in %.16g seconds\n", secs);
-   	    break;
-   	    
-   	  default:
-		fprintf(stdout, "invalid option %d\n", num_test);
-			
-	}//end switch
-	
-	bdd_done();
-	
-}//end main
+void show_elapsed_time(struct timeval *t_fin, struct timeval *t_ini)
+{
+	double secs;
+
+	secs = timeval_diff(t_fin, t_ini);
+	std::cout << "time in: " << secs << "secs" << std::endl;
+}
