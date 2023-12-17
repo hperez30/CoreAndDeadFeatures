766e26b8177117f9ffb5dafe7ca632242f96adf2
hperez30 <hperez@issi.uned.es>
2023-12-17 12:12:58 +0100

    add utils

--- coreanddead/core_test/test.c
+++ coreanddead/core_test/test.c
@@ -88,20 +88,7 @@ int main(int argc, char **argv)
 
 } // end main
 
-/* return "a - b" in seconds */
-double timeval_diff(struct timeval *a, struct timeval *b) {
-	return (double)(a->tv_sec + (double)a->tv_usec / 1000000) -
-		   (double)(b->tv_sec + (double)b->tv_usec / 1000000);
-} // time_diff
-
 void show_results(int num_dead, int num_core) {
 	std::cout << "core: " << num_core << std::endl;
 	std::cout << "dead: " << num_dead << std::endl;
-}
-
-void show_elapsed_time(struct timeval *t_fin, struct timeval *t_ini){
-	double secs;
-
-	secs = timeval_diff(t_fin, t_ini);
-	std::cout << "time in: " << secs << "secs" << std::endl;
 }
