1de199ac677673ea322900a1138994484c8f3ac9
hperez30 <hperez@issi.uned.es>
2023-12-17 18:12:09 +0100

    add reading params

--- coreanddead/src/test.cpp
+++ coreanddead/src/test.cpp
@@ -2,39 +2,30 @@
 
 int main(int argc, char **argv)
 {	
-	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " <bdd file> <option method>" << std::endl;
-		exit(-1);
-	}
+	int method;	
 
-	std::ifstream mybdd(argv[1]);
-	int method = atoi(argv[2]);
-
-	if (!mybdd) {
-		std::cerr << "File " << argv[1] << " not found" << std::endl;
-		exit(-1);
+	if (read_params(argc, argv, &method)){
+		return 1;
 	}
 
 	/* Initilize buddy */
 	bdd_init(INITBDDNODES, INITBDDCACHE);
 	bdd_setmaxincrease(INITBDDMAXINCREASE);
+
 #ifdef REORDER
-	/* compile with debug flag if you want to use automatic reordering techniques */
+	/* compile with REORDER flag if you want to use automatic reordering techniques */
 	bdd_autoreorder(BDD_REORDER_SIFT);
 #endif
 
-	bdd solutionSpace;
-
-	bdd_fnload((char *)argv[1], solutionSpace);
-
-	std::cerr << "File " << argv[1] << " loaded" << std::endl;
+	bdd solutionSpace;	
+	bdd_fnload((char *)argv[2], solutionSpace);
+	std::cerr << "Model " << argv[1] << " loaded" << std::endl;
 
 	bdd_varblockall();
 	bdd_reorder(BDD_REORDER_SIFT);
 	
 	struct timeval t_ini, t_fin;
-	gettimeofday(&t_ini, NULL);
-	
+	gettimeofday(&t_ini, NULL);	
 	int *num_dead_core;
 
 	switch (method) {
@@ -58,14 +49,12 @@ int main(int argc, char **argv)
 			
 			free(num_dead_core);
 			break;
-		}
-			
+		}			
 
 		case 2: // our approach	
 		{
 			int *var_low = (int *)calloc(bdd_varnum(), sizeof(int));
 			int *var_high = (int *)calloc(bdd_varnum(), sizeof(int));
-
 			int *marks = (int *)calloc(bddnodesize, sizeof(int));
 			int *res_node = (int *)calloc(bddnodesize, sizeof(int));
 
@@ -87,10 +76,8 @@ int main(int argc, char **argv)
 		}					
 
 		default:
-			std::cout << "invalid option " << method << std::endl;
-
-	} // end switch
-
+			std::cout << "invalid " << method << " method " << std::endl;
+	} 
 	bdd_done();
 
 } // end main
@@ -98,4 +85,44 @@ int main(int argc, char **argv)
 void show_results(int num_dead, int num_core) {
 	std::cout << "core: " << num_core << std::endl;
 	std::cout << "dead: " << num_dead << std::endl;
+}
+
+int read_params(int argc, char **argv, int *method) {
+	int option;
+	std::string filePath;
+
+	while ((option = getopt(argc, argv, "f:m:")) != -1) {
+        switch (option) {
+            case 'f':
+				filePath = optarg;												               
+                break;
+			case 'm':
+				std::cerr << optarg << std::endl;
+                *method = atoi(optarg);
+                break;
+            case '?':
+                if (optopt == 'f') {
+                    std::cerr << "Option -f requires a valid bdd file path." << std::endl;
+                } else {
+					if (optopt == 'm') {
+						std::cerr << "Option -m requires a valid method (1) brute force approach (2) our method." << std::endl;
+					}
+					else {
+                    	std::cerr << "Uknown Option: -" << static_cast<char>(optopt) << std::endl;
+					}
+                }
+                return 1;
+            default:
+                std::cerr << "Usage: " << argv[0] << " -f <path_to_bdd_file>" << std::endl;
+                return 1;
+        }
+    }
+
+	std::ifstream mybdd(filePath);
+	if (!mybdd.is_open()) {
+		std::cerr << "The model could not be opened: " << filePath << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
